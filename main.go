15ef74ed22f3a72c4642c25b9759c653798e5e18
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-01-15 10:45:28 +0100

    fix all commit view - update version for tags

--- main.go
+++ main.go
@@ -1,105 +1,105 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "os"
-    // "regexp"
-    "strings"
+	"flag"
+	"fmt"
+	"os"
+
+	// "regexp"
+	"strings"
 )
 
-var VERSION = "0.0.5"
+var VERSION = "0.0.6"
 
 func main() {
-    projectPath := flag.String("path", ".", "Chemin du projet")
-    uiMode := flag.Bool("ui", false, "Lancer l'interface utilisateur")
-    version := flag.Bool("version", false, "Afficher la version")
-    v := flag.Bool("v", false, "Afficher la version")
-    //Setup args
-    installCmd := flag.Bool("install", false, "Installer les submodules")
-    branchArg := flag.String("branch", "", "Spécifier la ou les branches (séparées par un espace)")
-    npmCmd := flag.Bool("npm", false, "Installer les dépendances npm")
-    fullCmd := flag.Bool("full", false, "Installation complète (submodules + npm)")
-    devopsCmd := flag.Bool("ui-devops", false, "Lancer l'interface DevOps")
+	projectPath := flag.String("path", ".", "Chemin du projet")
+	uiMode := flag.Bool("ui", false, "Lancer l'interface utilisateur")
+	version := flag.Bool("version", false, "Afficher la version")
+	v := flag.Bool("v", false, "Afficher la version")
+	//Setup args
+	installCmd := flag.Bool("install", false, "Installer les submodules")
+	branchArg := flag.String("branch", "", "Spécifier la ou les branches (séparées par un espace)")
+	npmCmd := flag.Bool("npm", false, "Installer les dépendances npm")
+	fullCmd := flag.Bool("full", false, "Installation complète (submodules + npm)")
+	devopsCmd := flag.Bool("ui-devops", false, "Lancer l'interface DevOps")
 	flag.Parse()
 
-    if err := checkForUpdates(VERSION); err != nil {
-        fmt.Println("Erreur lors de la vérification des mises à jour:", err)
-    }
+	if err := checkForUpdates(VERSION); err != nil {
+		fmt.Println("Erreur lors de la vérification des mises à jour:", err)
+	}
 
-
-        // Traitement des commandes d'installation
-    if *installCmd {
-        if *branchArg != "" {
-            // Diviser l'argument branch en cas de branches multiples
-            branches := strings.Fields(*branchArg)
-            fmt.Printf("Installer les sous-modules avec les branches : %s\n", branches)
-            if err := submoduleAction(branches...); err != nil {
-                fmt.Printf("Erreur: %v\n", err)
-                os.Exit(1)
-            }
-            if *npmCmd {
-                if err := npmAction(true); err != nil {
-                    fmt.Printf("Erreur: %v\n", err)
-                    os.Exit(1)
-                }
-            }
-        } else if *fullCmd {
-            if err := submoduleAction(); err != nil {
-                fmt.Printf("Erreur: %v\n", err)
-                os.Exit(1)
-            }
-            if err := npmAction(true); err != nil {
-                fmt.Printf("Erreur: %v\n", err)
-                os.Exit(1)
-            }
-        } else if *npmCmd {
-            if err := npmAction(true); err != nil {
-                fmt.Printf("Erreur: %v\n", err)
-                os.Exit(1)
-            }
-        } else {
-            if err := submoduleAction(); err != nil {
-                fmt.Printf("Erreur: %v\n", err)
-                os.Exit(1)
-            }
-        }
-        return
-    }
+	// Traitement des commandes d'installation
+	if *installCmd {
+		if *branchArg != "" {
+			// Diviser l'argument branch en cas de branches multiples
+			branches := strings.Fields(*branchArg)
+			fmt.Printf("Installer les sous-modules avec les branches : %s\n", branches)
+			if err := submoduleAction(branches...); err != nil {
+				fmt.Printf("Erreur: %v\n", err)
+				os.Exit(1)
+			}
+			if *npmCmd {
+				if err := npmAction(true); err != nil {
+					fmt.Printf("Erreur: %v\n", err)
+					os.Exit(1)
+				}
+			}
+		} else if *fullCmd {
+			if err := submoduleAction(); err != nil {
+				fmt.Printf("Erreur: %v\n", err)
+				os.Exit(1)
+			}
+			if err := npmAction(true); err != nil {
+				fmt.Printf("Erreur: %v\n", err)
+				os.Exit(1)
+			}
+		} else if *npmCmd {
+			if err := npmAction(true); err != nil {
+				fmt.Printf("Erreur: %v\n", err)
+				os.Exit(1)
+			}
+		} else {
+			if err := submoduleAction(); err != nil {
+				fmt.Printf("Erreur: %v\n", err)
+				os.Exit(1)
+			}
+		}
+		return
+	}
 	if *uiMode {
-        submodules, err := listSubmodule(*projectPath)
-        if err != nil {
-            fmt.Println("Erreur :", err)
-            return
-        }
-        // Appelle cleanSubmodule pour nettoyer les chemins
-        submoduleNames, err := cleanSubmodule(submodules)
-        if err != nil {
-            fmt.Println("Erreur :", err)
-            return
-        }
+		submodules, err := listSubmodule(*projectPath)
+		if err != nil {
+			fmt.Println("Erreur :", err)
+			return
+		}
+		// Appelle cleanSubmodule pour nettoyer les chemins
+		submoduleNames, err := cleanSubmodule(submodules)
+		if err != nil {
+			fmt.Println("Erreur :", err)
+			return
+		}
 		// Lancer l'interface utilisateur
 		RunUI(submodules, submoduleNames)
 	} else if *version || *v {
-        fmt.Println("Aidalinfo devcli version", VERSION)
-    } else if *devopsCmd {
-        // Appelle cleanSubmodule pour nettoyer les chemins
-        submodules, err := listSubmodule(*projectPath)
-        submoduleNames, err := cleanSubmodule(submodules)
-        if err != nil {
-            fmt.Println("Erreur :", err)
-            return
-        }
-        RunDevOpsUI(submodules, submoduleNames)
-    } else {
-        fmt.Println("Usage:")
-        fmt.Println("  -ui              Lancer l'interface utilisateur")
-        fmt.Println("  -ui-devops       Lancer l'interface DevOps (only tags pour le moment)")
-        fmt.Println("  -path            Spécifier le chemin du projet")
-        fmt.Println("  -install         Installer les submodules")
-        fmt.Println("  -branch=\"X Y\"    Spécifier la ou les branches (X avec fallback sur Y)")
-        fmt.Println("  -npm             Installer les dépendances npm")
-        fmt.Println("  -full            Installation complète (submodules + npm)")
-        fmt.Println("  -version, -v     Afficher la version")
+		fmt.Println("Aidalinfo devcli version", VERSION)
+	} else if *devopsCmd {
+		// Appelle cleanSubmodule pour nettoyer les chemins
+		submodules, err := listSubmodule(*projectPath)
+		submoduleNames, err := cleanSubmodule(submodules)
+		if err != nil {
+			fmt.Println("Erreur :", err)
+			return
+		}
+		RunDevOpsUI(submodules, submoduleNames)
+	} else {
+		fmt.Println("Usage:")
+		fmt.Println("  -ui              Lancer l'interface utilisateur")
+		fmt.Println("  -ui-devops       Lancer l'interface DevOps (only tags pour le moment)")
+		fmt.Println("  -path            Spécifier le chemin du projet")
+		fmt.Println("  -install         Installer les submodules")
+		fmt.Println("  -branch=\"X Y\"    Spécifier la ou les branches (X avec fallback sur Y)")
+		fmt.Println("  -npm             Installer les dépendances npm")
+		fmt.Println("  -full            Installation complète (submodules + npm)")
+		fmt.Println("  -version, -v     Afficher la version")
 	}
 }
